e7fac4df005ca6d15ba0bf13b0a1f82d51896a13
Vincent Prat <vpratfr@gmail.com>
2026-04-11 13:31:36 +0200

    Allow lazy fetching (only for fonts not already fetched)

--- src/Commands/FetchGoogleFontsCommand.php
+++ src/Commands/FetchGoogleFontsCommand.php
@@ -7,20 +7,22 @@ use Spatie\GoogleFonts\GoogleFonts;
 
 class FetchGoogleFontsCommand extends Command
 {
-    public $signature = 'google-fonts:fetch';
+    public $signature = 'google-fonts:fetch {--new-fonts-only : only download the fonts which are not already cached}';
 
     public $description = 'Fetch Google Fonts and store them on a local disk';
 
     public function handle()
     {
+        $newFontsOnly = $this->option('new-fonts-only') ?? false;
+        
         $this->info('Start fetching Google Fonts...');
 
         collect(config('google-fonts.fonts'))
             ->keys()
-            ->each(function (string $font) {
+            ->each(function (string $font) use ($newFontsOnly) {
                 $this->info("Fetching `{$font}`...");
 
-                app(GoogleFonts::class)->load(compact('font'), forceDownload: true);
+                app(GoogleFonts::class)->load(compact('font'), forceDownload: !$newFontsOnly);
             });
 
         $this->info('All done!');
